853d98c646d714f423571982039195f3e4fef369
Alexandre <alexandre.bobis@algosup.com>
2022-10-10 18:06:01 +0200

    Add test

--- geom/geom_test.go
+++ geom/geom_test.go
@@ -11,14 +11,14 @@ func TestPerimeter(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{"Perimeter 5 ", Pentagone{5, 3}, 25},
-		{"Perimeter 2.5 ", Circle{2.5}, 15.707963267948966},
 		{"Perimeter 4 and 6", Rectangle{4, 6}, 20},
 		{"Perimeter 7 and 9", Rectangle{7, 9}, 32},
+		{"Perimeter 2.5 ", Circle{2.5}, 15.707963267948966},
+		{"Perimeter 5 ", Pentagone{5, 3}, 25},
 		{"Perimeter 4", Hexagone{4}, 24},
 		{"Perimeter 12", Hexagone{12}, 72},
 		{"Perimeter 3, 4 and 6", Triangle{3, 4, 6, 0, 0}, 13},
-		{"perimeter 5 and 5", Star{5}, 50},
+		{"Perimeter 5", Star{5}, 50},
 	}
 
 	for _, c := range cases {
@@ -37,14 +37,14 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{"Area 5 and 3", Pentagone{5, 3}, 37.5},
 		{"Area 4 and 6", Rectangle{4, 6}, 24},
 		{"Area 7 and 9", Rectangle{7, 9}, 63},
 		{"Area 5.1", Circle{5.1}, 81.71282},
+		{"Area 5 and 3", Pentagone{5, 3}, 37.5},
 		{"Area 4", Hexagone{4}, 41.569219381653056},
 		{"Area 12", Hexagone{12}, 374.1229744348775},
 		{"Area 3 and 4", Triangle{0, 0, 0, 3, 4}, 6},
-		{"Area 5 and 5", Star{5}, 250},
+		{"Area 5", Star{5}, 250},
 	}
 
 	for _, c := range cases {
--- wallet/Aegla/aegla_test.go
+++ wallet/Aegla/aegla_test.go
@@ -1,3 +1,17 @@
 package wallet_aegla
 
-// import "testing"
+import "testing"
+
+func TestWallet(t *testing.T) {
+
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+	
+	if got != want {
+		t.Error("got %d want %d", got, want)
+	}
+}
