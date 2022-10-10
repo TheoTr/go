5cb4d709d9a507cbd831ad79ff1fc56f5b4005ba
Alexandre <alexandre.bobis@algosup.com>
2022-10-10 09:47:56 +0200

    push fork

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,5 +1,32 @@
 package triangle
 
-func Triangle() {
+type Triangle struct {
+	side1  float64
+	side2  float64
+	side3  float64
+	height float64
+	base   float64
+}
 
+// Perimeter calculates the perimeter of givens width and height of a triangle.
+func (t Triangle) Perimeter() float64 {
+	return (t.side1 + t.side2 + t.side3)
+}
+
+type Shape interface {
+	Perimeter() float64
+	Area() float64
+}
+
+func Perimeter(s Shape) float64 {
+	return s.Perimeter()
+}
+
+// Area calculates the area of givens width and height of a triangle.
+func (t Triangle) Area() float64 {
+	return ((t.base * t.height) / 2)
+}
+
+func Area(s Shape) float64 {
+	return s.Area()
 }
--- triangle/triangle_test.go
+++ triangle/triangle_test.go
@@ -2,6 +2,20 @@ package triangle
 
 import "testing"
 
-func Test_Triangle(t *testing.T) {
+func TestPerimeterTriangle(t *testing.T) {
+	t.Run("Triangle perimeter 3, 4 and 6", func(t *testing.T) {
+		got := Perimeter(Triangle{3, 4, 6, 0, 0})
+		if got != 13 {
+			t.Error("It's not good !!!")
+		}
+	})
+}
 
+func TestAreaTriangle(t *testing.T) {
+	t.Run("Triangle area 3 and 4", func(t *testing.T) {
+		got := Area(Triangle{0, 0, 0, 3, 4})
+		if got != 6 {
+			t.Error("It's not good !!!")
+		}
+	})
 }
