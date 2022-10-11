3bf5e3245214a2febc049b2abd714fceaafbaa75
TheoTr <71769486+TheoTr@users.noreply.github.com>
2022-10-11 09:27:00 +0200

    Start wallet

---
+++ wallet/KAnt1n/KAnt1n_test.go
@@ -0,0 +1,1 @@
+package kAnt1n
--- wallet/lesgoat/lesgoat_test.go
+++ wallet/lesgoat/lesgoat_test.go
@@ -4,4 +4,12 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
+	wallet := Wallet{}
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+	if got != want {
+		t.Error("got %d want %d", got, want)
+	}
 }
