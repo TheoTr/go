cf0bcee7a7cdbf7c2c8925dfdfac4886b7ca1f7e
TheoTr <71769486+TheoTr@users.noreply.github.com>
2022-10-11 13:40:16 +0200

    Start Wallet

--- wallet/lesgoat/lesgoat.go
+++ wallet/lesgoat/lesgoat.go
@@ -1,5 +1,13 @@
 package wallet_lesgoat
 
-type Wallet struct{
-	
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
 }
