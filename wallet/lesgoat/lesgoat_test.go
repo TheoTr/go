93b35eedf7626d0847f25129937e169088d61e24
TheoTr <71769486+TheoTr@users.noreply.github.com>
2022-10-11 15:52:39 +0200

    Reform Test

--- wallet/lesgoat/lesgoat_test.go
+++ wallet/lesgoat/lesgoat_test.go
@@ -4,43 +4,28 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Satoshi(10))
-
+	assertBalance := func(t testing.TB, wallet Wallet, want Satoshi) {
+		t.Helper()
 		got := wallet.Balance()
-		want := Satoshi(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Satoshi(10))
+		assertBalance(t, wallet, Satoshi(10))
 	})
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Satoshi(20)}
-
 		wallet.Withdraw(Satoshi(10))
-
-		got := wallet.Balance()
-
-		want := Satoshi(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Satoshi(10))
 	})
 	t.Run("Outoflimit", func(t *testing.T) {
 		wallet := Wallet{}
-
 		wallet.Deposit(Satoshi(21_000_000_0000_0001))
-
-		got := wallet.Balance()
-
-		want := Satoshi(0)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Satoshi(0))
 	})
 
 }
