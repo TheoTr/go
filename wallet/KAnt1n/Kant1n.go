2cf092f86d1f8e00a5b565c773546807c9798dd2
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2022-10-11 15:14:09 +0200

    p

--- wallet/KAnt1n/Kant1n.go
+++ wallet/KAnt1n/Kant1n.go
@@ -1,3 +1,36 @@
 package wallet_KAnt1n
 
-type Wallet struct{}
+import "fmt"
+
+type Satoshi int
+
+func (s Satoshi) Validate() bool {
+	return s <= 21_000_000_0000_0000
+}
+
+type Stringer interface {
+	String() string
+}
+
+type Wallet struct {
+	balance Satoshi
+}
+
+func (w *Wallet) Deposit(amount Satoshi) {
+	if !amount.Validate() {
+		return
+	}
+	w.balance += amount
+}
+
+func (w Wallet) Balance() Satoshi {
+	return w.balance
+}
+
+func (b Satoshi) String() string {
+	return fmt.Sprintf("%d SATOSHI", b)
+}
+
+func (w *Wallet) Withdraw(amount Satoshi) {
+	w.balance -= amount
+}
