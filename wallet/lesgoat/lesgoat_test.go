de0e740af25a38c924e198d34cd4d38bf4c0e6c1
TheoTr <71769486+TheoTr@users.noreply.github.com>
2022-10-11 15:00:40 +0200

    Test Outoflimit

--- wallet/lesgoat/lesgoat_test.go
+++ wallet/lesgoat/lesgoat_test.go
@@ -29,5 +29,18 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	})
+	//t.Run("Outoflimit", func(t *testing.T) {
+	//	wallet := Wallet{}
+
+	//	wallet.Deposit(Satoshi(21_000_000_0000_0001))
+
+	//	got := wallet.Balance()
+
+	//	want := Satoshi(0)
+
+	//	if got != want {
+	//		t.Errorf("got %s want %s", got, want)
+	//	}
+	//})
 
 }
