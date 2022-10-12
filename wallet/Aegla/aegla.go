c5a651aac512d3d839824e21fc93788e0102cd11
Alexandre <alexandre.bobis@algosup.com>
2022-10-12 13:42:25 +0200

    Refactor aegla package

--- wallet/Aegla/aegla.go
+++ wallet/Aegla/aegla.go
@@ -1,5 +1,45 @@
 package wallet_aegla
 
-type Wallet struct{
-	
+import (
+	"errors"
+	"fmt"
+)
+
+type Satoshi uint64
+
+func (s Satoshi) Validate() bool {
+	return s <= 21_000_000_0000_0000
+}
+
+type Stringer interface {
+	String() string
+}
+
+type Wallet struct {
+	balance Satoshi
+}
+
+func (w *Wallet) Deposit(amount Satoshi) {
+	if !amount.Validate() {
+		return
+	}
+	w.balance += amount
+}
+
+func (w Wallet) Balance() Satoshi {
+	return w.balance
+}
+
+func (s Satoshi) String() string {
+	bitcoins := s / 1_000_000_00
+	return fmt.Sprintf("%d BTC", bitcoins)
+}
+
+func (w *Wallet) Withdraw(amount Satoshi) error {
+	if amount > w.balance {
+		return errors.New("NOT ENOUGH MONEY")
+	}
+
+	w.balance -= amount
+	return nil
 }
--- wallet/Aegla/aegla_test.go
+++ wallet/Aegla/aegla_test.go
@@ -1,17 +1,44 @@
-package wallet_aegla
+package wallet_aegla 
 
 import "testing"
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
+	assertBalance := func(t testing.TB, wallet Wallet, want Satoshi) {
+		t.Helper()
+		got := wallet.Balance()
 
-	wallet.Deposit(10)
-
-	got := wallet.Balance()
-	want := 10
-	
-	if got != want {
-		t.Error("got %d want %d", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Satoshi(10))
+		assertBalance(t, wallet, Satoshi(10))
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Satoshi(20)}
+		wallet.Withdraw(Satoshi(10))
+		assertBalance(t, wallet, Satoshi(10))
+	})
+	t.Run("Outoflimit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Satoshi(21_000_000_0000_0001))
+		assertBalance(t, wallet, Satoshi(0))
+	})
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Satoshi(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Satoshi(100))
+
+		assertError(t, err)
+		assertBalance(t, wallet, startingBalance)
+	})
 }
